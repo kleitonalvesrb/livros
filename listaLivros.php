22fcbfc3c649003a11d429fa072b9979198b198d
kleitonbatista <kleiton.a.batista@gmail.com>
2019-01-29 18:02:44 -0200

    imagens livros

--- listaLivros.php
+++ listaLivros.php
@@ -1,5 +1,5 @@
  <?php
-    require_once "controller/Livro.php";?>
+require_once "controller/Livro.php";?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,97 +7,66 @@
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <!------ Include the above in your HEAD tag ---------->
-
-    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
-
     <head>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons">
         <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossorigin="anonymous">
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons">
     </head>
-
     <style type="text/css">
         body {
             color: #fff;
             background: #d47677;
         }
-        #custom-search-input{
-            padding: 3px;
-            border: solid 1px #E4E4E4;
-            border-radius: 6px;
-            background-color: #fff;
-        }
-
-        #custom-search-input input{
-            border: 0;
-            box-shadow: none;
-        }
-
-        #custom-search-input button{
-            margin: 2px 0 0 0;
-            background: none;
-            box-shadow: none;
-            border: 0;
-            color: #666666;
-            padding: 0 8px 0 10px;
-            border-left: solid 1px #ccc;
-        }
-
-        #custom-search-input button:hover{
-            border: 0;
-            box-shadow: none;
-            border-left: solid 1px #ccc;
-        }
-
-        #custom-search-input .glyphicon-search{
-            font-size: 23px;
-        }
-
     </style>
 </head>
-<body>
+<body id="page-top">
 
-<div class="container mt-5">
-    <div class="col-md-offset-6">
-        <div class="title">
-            <h3>Livros</h3>
+<nav class="navbar navbar-expand navbar-dark bg-dark static-top">
+    <a class="navbar-brand mr-1" href="listaLivros.php">Esse eu já lí!</a>
+    <!-- Navbar Search -->
+    <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
+        <div class="input-group">
+            <!--<input type="text" class="form-control" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">-->
         </div>
-    </div>
+    </form>
 
+    <li><a href="#"><i class="glyphicon glyphicon-off"></i> Logout</a></li>
+</nav>
+</body>
+<p>
+<body>
+<div class="container mt-5">
+    <div class="title">
+        <h3>Images</h3>
+    </div>
+    <input type="text" class="">
     <div class="row">
         <?php
-            $livros = new Livro();
-            echo $livros->listaTodosLivros();
+        $livros = new Livro();
+        echo $livros->listaTodosLivros();
         ?>
-<!--        <div class="col-md-3">-->
-<!--            <h4>Rounded Image</h4>-->
-<!--            <img src="imagens/livros.jpeg" alt="Rounded Image" class="rounded img-fluid">-->
-<!--        </div>-->
-<!---->
-<!--        <div class="col-md-3">-->
-<!--            <h4>Rounded Image</h4>-->
-<!--            <img src="imagens/livros.jpeg" alt="Rounded Image" class="rounded img-fluid">-->
-<!--        </div>-->
-<!---->
-<!--        <div class="col-md-3">-->
-<!--            <h4>Rounded Image</h4>-->
-<!--            <img src="imagens/livros.jpeg" alt="Rounded Image" class="rounded img-fluid">-->
-<!--        </div>-->
-<!---->
-<!--        <div class="col-md-3">-->
-<!--            <h4>Rounded Image</h4>-->
-<!--            <img src="imagens/livros.jpeg" alt="Rounded Image" class="rounded img-fluid">-->
-<!--        </div>-->
-<!---->
-<!--        <div class="col-md-3">-->
-<!--            <h4>Rounded Image</h4>-->
-<!--            <img src="imagens/livros.jpeg" alt="Rounded Image" class="rounded img-fluid">-->
-<!--        </div>-->
-<!--    </div>-->
 
-</div>
+
+    </div>
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-primary" href="login.html">Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </body>
 </html>
